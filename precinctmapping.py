87bb46ce06bc04cf1b40c911cdd8e3f5cf4bfff9
Zhuoming2017 <dahlia.dry24@gmail.com>
2020-05-19 20:50:06 -0400

    second commit

--- precinctmapping.py
+++ precinctmapping.py
@@ -15,30 +15,39 @@ from bokeh.io import export_png
 
 class Chloropleth(object):
     """
+    Make Chloropleth maps to help visualize demographic/electoral data by precinct.
+    Creates interactive figures using bokeh in Jupyter Notebooks, hoverable to see exact data values for each precinct
 
+    Parameters:
+    --json_data (JSON-formatted str): input data, consisting of merged geopandas shapefile data
+        and pandas input data
+    --varname (str): Name of the column in the input data that you want to map
+    --palette (bokeh palette): Color scheme for the map. Great tool for picking palettes is here -
+        https://colorbrewer2.org/#type=sequential&scheme=BuGn&n=3
+    --figtitle (str): What you want the displayed title of the map to be
+    --mapscale (tuple): Tuple in the form (min,max) where min, max are the highest and lowest data values
+        scaled to the color palette
+
+    Functions:
+        create(): builds the map and displays it using bokeh in a Jupyter notebook
     """
-    def __init__(self, json_data, varname, var, figtitle, palette): #eg. palette = brewer['YlGnBu'][8][::-1]
+    def __init__(self, json_data, varname, figtitle, palette, mapscale): #eg. palette = brewer['YlGnBu'][8][::-1]
         self.json_data = json_data
         self.varname = varname
-        self.var = var
         self.palette = palette
         self.figtitle = figtitle
+        self.mapscale = mapscale
 
-    def create(self, fixed = False):
+    def create(self):
         # Input GeoJSON source that contains features for plotting.
         geosource = GeoJSONDataSource(geojson=self.json_data)
         # Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
-        if not fixed:
-            color_mapper = LinearColorMapper(palette=self.palette, nan_color = '#d9d9d9',low=min(self.var), high=max(self.var))
-            color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8, width=int(400 * 1.5), height=20,
-                                 border_line_color=None, location=(0, 0), orientation='horizontal')
-        else:
-            color_mapper = LinearColorMapper(palette=self.palette, low=-16, high=4,nan_color = '#d9d9d9')
-            tick_labels = {'-16': '<-16', '4':'>4'}
-            color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8, width=int(400*1.5), height=20,
+        color_mapper = LinearColorMapper(palette=self.palette, low=self.mapscale[0], high=self.mapscale[1],nan_color = '#d9d9d9')
+        tick_labels = {str(self.mapscale[0]): '<' + str(self.mapscale[0]), str(self.mapscale[1]):'>' + str(self.mapscale[1])}
+        color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8, width=int(400*1.5), height=20,
                                  border_line_color=None, location=(0, 0), orientation='horizontal',major_label_overrides=tick_labels)
         # Add hover tool
-        hover = HoverTool(tooltips=[('County', '@CNTY'), ('Precinct', '@Precinct'), (self.varname,'@'+ self.varname)])
+        hover = HoverTool(tooltips=[('County', '@CNTY'), ('Precinct', '@Precinct'),(self.varname,'@'+self.varname)])
         # Create figure object.
         p = figure(title=self.figtitle, plot_height=int(600*1.5), plot_width=int(500*1.5),
                    toolbar_location=None, tools=[hover])
